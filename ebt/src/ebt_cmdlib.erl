3094bac93908fa7300c0f3480b8cff7e9c2a4974
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2013-07-05 04:51:53 +0300

    new exec lib

--- ebt/src/ebt_cmdlib.erl
+++ ebt/src/ebt_cmdlib.erl
@@ -32,20 +32,16 @@
 %% API
 -export([exec/2, exec/1]).
 
-exec({Command, Params}) ->
-    exec(ebt__xl_string:format(Command, Params));
+exec({Command, Params}) -> exec(ebt__xl_string:format(Command, Params));
 exec(Command) ->
     io:format("exec ~s~n", [Command]),
-    process_result(Command, ebt__xl_shell:command(Command)).
+    process_result(Command, ebt__xl_shell:exec(Command)).
 
-exec({Command, Params}, Dir) ->
-    exec(ebt__xl_string:format(Command, Params), Dir);
+exec({Command, Params}, Dir) -> exec(ebt__xl_string:format(Command, Params), Dir);
 exec(Command, Dir) ->
     io:format("exec ~s in ~s~n", [Command, Dir]),
-    process_result(Command, ebt__xl_shell:command(Command, Dir)).
+    process_result(Command, ebt__xl_shell:exec(Command, Dir)).
 
-process_result(_Command, {ok, Stdout}) ->
-    io:format("~s", [Stdout]);
-process_result(Command, {error, Stdout}) ->
-    io:format("~s", [Stdout]),
-    {error, ebt__xl_string:format("failed: ~s", [Command])}.
+process_result(_Command, ok) -> ok;
+process_result(Command, {error, Exit}) ->
+    {error, ebt__xl_string:format("command ~s failed with exit ~p", [Command, Exit])}.
