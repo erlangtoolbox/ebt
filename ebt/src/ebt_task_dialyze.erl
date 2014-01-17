20d22c64e53319eb8c18d6c2a36e6529cf6b974d
Volodymyr Kyrychenko <volodymyr.kyrychenko@strikead.com>
2014-01-17 13:21:43 +0200

    cleanup

--- ebt/src/ebt_task_dialyze.erl
+++ ebt/src/ebt_task_dialyze.erl
@@ -27,13 +27,6 @@
 %%  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 %%  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-%%
-%%  All rights reserved.
-%%
-%%  Redistribution and use in source and binary forms, with or without
-%%  modification, are permitted provided that the following conditions are met:
-%%
-%%
 %% @doc Dialyze
 %%
 %% == Configuration ==
